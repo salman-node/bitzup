ce3985f99a3d5b5d7a1588603b2a144727c1debb
salman-node <salman@suncrypto.in>
2024-12-27 04:59:41 +0000

    refresh push

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    apps: [
+      {
+        name: "zookeeper",
+        script: "./zookeeper-server-start.sh",
+        args: "../config/zookeeper.properties",
+        cwd: "/root/kafka_2.13-3.9.0/bin",
+        autorestart: true,
+      },
+      {
+        name: "kafka",
+        script: "./kafka-server-start.sh",
+        args: "../config/server.properties",
+        cwd: "/root/kafka_2.13-3.9.0/bin",
+        autorestart: true,
+      }
+    ]
+  };
+  
